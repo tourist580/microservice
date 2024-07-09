296659a3f107c7a60a801cbc8d9949832a9d0092
dongxuandanh <xuandanh0401@gmail.com>
2024-07-09 23:37:48 +0700

    Init project

---
+++ auth-service/account-server/src/main/java/com/spring/microservice/authservice/AccountServerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.microservice.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountServerApplication.class, args);
+	}
+
+}
